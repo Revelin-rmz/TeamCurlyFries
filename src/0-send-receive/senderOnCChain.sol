95af64ddd1c3dc90dbf3f66333a0d922b997b70a
Yousif2326 <yousif.salman2327@gmail.com>
2024-05-17 22:13:54 -0400

    avalanche-starter-kit

---
+++ src/0-send-receive/senderOnCChain.sol
@@ -0,0 +1,29 @@
+// (c) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: Ecosystem
+
+pragma solidity ^0.8.18;
+
+import "@teleporter/ITeleporterMessenger.sol";
+
+contract SenderOnCChain {
+    ITeleporterMessenger public immutable messenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
+
+    /**
+     * @dev Sends a message to another chain.
+     */
+    function sendMessage(address destinationAddress, string calldata message) external {
+        messenger.sendCrossChainMessage(
+            TeleporterMessageInput({
+                // Replace with blockchainID of your Subnet (see instructions in Readme)
+                destinationBlockchainID: 0xa1adcd8b9ac1d5665494c4b8a830594b6486213aef8cfdb8beee61a31eae793f,
+                destinationAddress: destinationAddress,
+                feeInfo: TeleporterFeeInfo({feeTokenAddress: address(0), amount: 0}),
+                requiredGasLimit: 100000,
+                allowedRelayerAddresses: new address[](0),
+                message: abi.encode(message)
+            })
+        );
+    }
+}
